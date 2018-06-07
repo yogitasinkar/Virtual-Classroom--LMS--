f373551468b725a3ec453d8580e12ee6809aea39
yogitasinkar <35833826+yogitasinkar@users.noreply.github.com>
2018-06-07 17:56:00 +0530

    Update header.php

--- virtualcl/header.php
+++ virtualcl/header.php
@@ -6,8 +6,7 @@
   <head>
     <meta charset="utf-8">
     <title>Virtual Classroom</title>
-    <link rel="stylesheet" href="assets\bootstrap.min.css">
-
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <script src="https://use.fontawesome.com/911fcf40cf.js"></script>
